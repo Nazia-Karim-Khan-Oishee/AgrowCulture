01b37e8a79699c0eeb076806c1672f809dd3447f
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2022-09-17 02:15:05 +0600

    Change Password

--- Createaccloginpage/ChangePassword.php
+++ Createaccloginpage/ChangePassword.php
@@ -8,23 +8,61 @@ session_start();
 if (isset($_POST['submit'])) 
 {
     $user_name = $_POST['user_name'];
+    $currentpass = $_POST['currentpass'];
+    $checkpassword = $_POST['new_password'];
     $new_password = md5($_POST['new_password']);
     $cpassword = md5($_POST['cpassword']);
+    $uppercase = preg_match('@[A-Z]@', $checkpassword);
+    $lowercase = preg_match('@[a-z]@', $checkpassword);
+    $number    = preg_match('@[0-9]@', $checkpassword);
+    $specialchars = preg_match('@[^\w]@', $checkpassword);
+    $Validate= true;
+    if(!$uppercase || !$lowercase || !$number || !$specialchars || strlen($checkpassword)<5 ) {
+        $Validate= false;
+      }
+
+    if($Validate)
+    {
     $sql = "SELECT * FROM users WHERE user_name = '$user_name'";
     $result = mysqli_query($Conn, $sql);
     if ($result->num_rows > 0) 
-    {
-        //$sql2= "DELETE password FROM users where users.user_name = '$user_name'";
+    //{
+       // $sql3 = "SELECT password FROM users WHERE user_name = '$user_name'";
+       // $verify = password_verify($checkpassword, $sql3);
+
+       // $result3 = mysqli_query($Conn , $sql3);
+       // if($verify)
+       {
+        
+        if($new_password == $cpassword)
+           {
             $sql2 = "UPDATE users SET password = '$new_password' WHERE user_name = '$user_name' ";
             $result2 = mysqli_query($Conn , $sql2);
             header("Location: INDEX.php");
+           } 
+           else
+           {
+            echo "<script>alert('Password does not match')</script>";
+           }
+        }
+       // else
+        //{
+        //    echo "<script>alert('Current Password does not match')</script>";
+       // }
 
-    }
+   // }
             else 
             {
-                echo "<script>alert('User not found.');</script>";
+                echo "<p class='er'><big>User not found.</big>.</big></p>";
+
             }
         }
+        else{
+            echo "<p class='er'><big>Password should contain at least one 
+    uppercase letter, one lowercase letter, one special character and one number </big>.</big></p>";
+
+        }
+    }
 ?>
 
 
@@ -42,6 +80,10 @@ if (isset($_POST['submit']))
             <div class="form__message form__message--error"></div>
             <div class="form__input-group">
                 <input type="text" class="form__input" name="user_name" autofocus placeholder="Username" value="<?php echo $user_name; ?>" required>
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="text" class="form__input" name="currentpass" autofocus placeholder="Current Password" value="<?php echo $currentpass; ?>" required>
                 <div class="form__input-error-message"></div>
             </div>
             <div class="form__input-group">
