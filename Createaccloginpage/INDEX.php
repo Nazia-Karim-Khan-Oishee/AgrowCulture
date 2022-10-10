81e2c3d61b8077e87f67f6236d2481a5fd34a8f5
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2022-10-11 01:11:43 +0600

    Commit

--- Createaccloginpage/INDEX.php
+++ Createaccloginpage/INDEX.php
@@ -26,7 +26,7 @@ if (isset($_POST['submit'])) {
             $_POST['password'] = "";
             $_POST['user_name'] = "";
             unset($user_name);
-            header("Location: dashboard.php");
+            header("Location: Welcome.php");
         }
         else 
         {
