dd5db62d6b885a310d4d5d5b48ec3e686adca7f9
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2022-10-11 21:53:07 +0600

    Commit

--- Createaccloginpage/profile.php
+++ Createaccloginpage/profile.php
@@ -10,100 +10,100 @@ $query = "SELECT * FROM users WHERE user_name = '$user_name'";
 $req = mysqli_query($Conn , $query);
 $row = mysqli_fetch_assoc($req);
 $Name= $row['Name'];
-$Username = $row['user_name'];
-$Mobilenumber = $row['MobileNumber'];
+$user_name = $row['user_name'];
+$MobileNumber = $row['MobileNumber'];
 
-$currentpass = "";
-$checkpassword = "";
-$new_password = "";
-$cpassword = "";
-if (!isset($_SESSION['user_name'])) 
-{
-    header("Location: INDEX.php");
-}
-if(isset($_SESSION['Just_Set']) && $_SESSION['Just_Set']==true)
-{
-    echo "<script>alert('Wow! User Registration Completed.')</script>";
-    $SESSION['Just_Set'] = false;
-}
-if (isset($_POST['submit'])) 
-{
-    $user_name = $_SESSION['user_name'];
-    $currentpass = $_POST['currentpass'];
-    $checkpassword = $_POST['new_password'];
-    $new_password = md5($_POST['new_password']);
-    $cpassword = md5($_POST['cpassword']);
-    $uppercase = preg_match('@[A-Z]@', $checkpassword);
-    $lowercase = preg_match('@[a-z]@', $checkpassword);
-    $number    = preg_match('@[0-9]@', $checkpassword);
-    $specialchars = preg_match('@[^\w]@', $checkpassword);
-    $Validate= true;
-    if(!$uppercase || !$lowercase || !$number || !$specialchars || strlen($checkpassword)<5 ) {
-        $Validate= false;
-    }
+// $currentpass = "";
+// $checkpassword = "";
+// $new_password = "";
+// $cpassword = "";
+// if (!isset($_SESSION['user_name'])) 
+// {
+//     header("Location: INDEX.php");
+// }
+// if(isset($_SESSION['Just_Set']) && $_SESSION['Just_Set']==true)
+// {
+//     echo "<script>alert('Wow! User Registration Completed.')</script>";
+//     $SESSION['Just_Set'] = false;
+// }
+// if (isset($_POST['submit'])) 
+// {
+//     $user_name = $_SESSION['user_name'];
+//     $currentpass = $_POST['currentpass'];
+//     $checkpassword = $_POST['new_password'];
+//     $new_password = md5($_POST['new_password']);
+//     $cpassword = md5($_POST['cpassword']);
+//     $uppercase = preg_match('@[A-Z]@', $checkpassword);
+//     $lowercase = preg_match('@[a-z]@', $checkpassword);
+//     $number    = preg_match('@[0-9]@', $checkpassword);
+//     $specialchars = preg_match('@[^\w]@', $checkpassword);
+//     $Validate= true;
+//     if(!$uppercase || !$lowercase || !$number || !$specialchars || strlen($checkpassword)<5 ) {
+//         $Validate= false;
+//     }
     
-    if($Validate)
-    {
-      $sql3 = "SELECT password FROM users WHERE user_name = '$user_name'";
-       $result3 = mysqli_query($Conn , $sql3);
-       $row=mysqli_fetch_assoc($result3);
-      $hash = password_hash($row['password'], PASSWORD_DEFAULT);
-      if(password_verify(md5($_POST['currentpass']),  $hash))
-       {
+//     if($Validate)
+//     {
+//       $sql3 = "SELECT password FROM users WHERE user_name = '$user_name'";
+//        $result3 = mysqli_query($Conn , $sql3);
+//        $row=mysqli_fetch_assoc($result3);
+//       $hash = password_hash($row['password'], PASSWORD_DEFAULT);
+//       if(password_verify(md5($_POST['currentpass']),  $hash))
+//        {
 
-            if($new_password == $cpassword)
-            {
-                $sql2 = "UPDATE users SET password = '$new_password' WHERE user_name = '$user_name' ";
-                $result2 = mysqli_query($Conn , $sql2);
-                echo "<p class='er'><big>Password Changed.</big></p>";
-                //header("Location: INDEX.php");
-            } 
-            else
-            { 
-                echo "<script>alert('Password does not match')</script>";
-            }
-       }
-     else
-        {
-            echo "<script>alert('Current Password does not match')</script>";
+//             if($new_password == $cpassword)
+//             {
+//                 $sql2 = "UPDATE users SET password = '$new_password' WHERE user_name = '$user_name' ";
+//                 $result2 = mysqli_query($Conn , $sql2);
+//                 echo "<p class='er'><big>Password Changed.</big></p>";
+//                 //header("Location: INDEX.php");
+//             } 
+//             else
+//             { 
+//                 echo "<script>alert('Password does not match')</script>";
+//             }
+//        }
+//      else
+//         {
+//             echo "<script>alert('Current Password does not match')</script>";
             
-         }
-        }
-        else{
-            echo "<p class='er'><big>Password should contain at least one 
-    uppercase letter, one lowercase letter, one special character and one number </big>.</big></p>";
+//          }
+//         }
+//         else{
+//             echo "<p class='er'><big>Password should contain at least one 
+//     uppercase letter, one lowercase letter, one special character and one number </big>.</big></p>";
 
-        }
-        unset($currpass);
-        unset($new_password);
-        unset($cpassword);
-    }
-    if (isset($_POST['submit2'])) 
-    {    
-        $user_name = $_SESSION['user_name'];
-        $MobileNumber = $_POST['MobileNumber'];
-        if(strlen($MobileNumber) != 11 )
-        {
-        echo "<script>alert('Invalid Mobile Number.')</script>";
+//         }
+//         unset($currpass);
+//         unset($new_password);
+//         unset($cpassword);
+//     }
+//     if (isset($_POST['submit2'])) 
+//     {    
+//         $user_name = $_SESSION['user_name'];
+//         $MobileNumber = $_POST['MobileNumber'];
+//         if(strlen($MobileNumber) != 11 )
+//         {
+//         echo "<script>alert('Invalid Mobile Number.')</script>";
 
-    }
-    elseif(strlen($MobileNumber) == 11 ) 
-    {
+//     }
+//     elseif(strlen($MobileNumber) == 11 ) 
+//     {
         
-        $sql4 = "UPDATE users SET MobileNumber = '$MobileNumber' WHERE user_name = '$user_name' ";
-        $result4 = mysqli_query($Conn , $sql4);
-        if($result4)
-        {           
-            echo "<script>alert('Mobile Number Changed.')</script>";
+//         $sql4 = "UPDATE users SET MobileNumber = '$MobileNumber' WHERE user_name = '$user_name' ";
+//         $result4 = mysqli_query($Conn , $sql4);
+//         if($result4)
+//         {           
+//             echo "<script>alert('Mobile Number Changed.')</script>";
             
-        }
-        else 
-        {
-            echo "<script>alert('Something went wrong.')</script>";
-        }
-    }
-    unset($MobileNumber);
-}
+//         }
+//         else 
+//         {
+//             echo "<script>alert('Something went wrong.')</script>";
+//         }
+//     }
+//     unset($MobileNumber);
+// }
 //session_destroy();
     
 ?>
@@ -124,11 +124,14 @@ if (isset($_POST['submit']))
             <form action=""method="post">
                 <div class="inputBox">
                     <label for="full_name">Full Name:</label>
-                    <input type ="text" id="full_name" name="Name" required>
+                    <span class="error"> <?php echo $Name;?></span> <br>
+                  <!--  <input type ="text" id="full_name" name="Name" required>-->
                 </div>
                 <div class="inputBox">
                     <label for="full_name">User Name:</label>
-                    <input type ="text" id="user_name" name="user_name" required>
+                    <span class="error"> <?php echo $user_name;?></span>
+                    <br>
+                    <!--<input type ="text" id="user_name" name="user_name" required>-->
                 </div>
                <!-- <div class="inputBox">
                     <label for="email">Email:</label>
@@ -136,7 +139,8 @@ if (isset($_POST['submit']))
                 </div>-->
                 <div class="inputBox">
                     <label for="MobileNum">Mobile Number:</label>
-                    <input type ="text" id="MobileNum" name="MobileNumber" required>
+                    <span class="error"> <?php echo $MobileNumber;?></span> 
+                   <!-- <input type ="text" id="MobileNum" name="MobileNumber" required>-->
                 </div>
                 <!--<div class="inputBox">
                 <label for="password">Change Password:</label>
