b93e1e8a40ba56bf3bfa9a137d1345d002e69692
sami <ittehadrahman@iut-dhaka.edu>
2022-10-30 18:38:22 +0600

    imagefetch

--- imagefetch.php
+++ imagefetch.php
@@ -1,67 +1,49 @@
-<?php
-$con = mysqli_connect("localhost","root","","image");
-
-    if(isset($_POST['upload']))
-    {
-        $file = $_FILES['image']['name'];
-
-        $query = "INSERT INTO upload(image) VALUES('$file')";
-        $res = mysqli_query($con, $query);
-        if($res)
-        {
-            move_uploaded_file($_FILES['image']['tmp_name'], "$file");
-        }
-    }
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<title>Store and retrieve image</title>
 </head>
 <body>
-    <div class="container">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-mid-6">
-                    <h3 class="text-center">UPLOAD IMAGE</h3>
+  
+  <form method="POST" action="index.php" enctype="multipart/form-data">
+  	  	
+  	  <input type="file" name="image">  	
+  	  <button type="submit" name="upload">Upload Image</button>
+  	
+  </form>
+  
+  <?php
+  // database connection
+  $conn = mysqli_connect("localhost", "root", "", "agrowculture");
+    
+  if (isset($_POST['upload'])) {
+	  
+  	// Get name of images
+  	$Get_image_name = $_FILES['image']['name'];
+  	
+  	// image Path
+  	$image_Path = "images/".basename($Get_image_name);
 
-                    <form class = "my-5" method = "post" enctype = "multipart/form-data"> 
-                        <input type="file" name="image" class="form-control">
-                        <input type="submit" name="upload" value="UPLOAD" class="btn btn-succes my-3">
-                    </form>
-                </div>
-                <div class="col-md-6">
-                <h3 class="text-center">DISPLAY IMAGE</h3>
-                <div class="my-3"></div>
-                <?php
-                        $sel = "SELECT image FROM upload";
-                        $que = mysqli_query($con, $sel);
-        
-                        $output = "";
-        
-                        if(mysqli_num_rows($que)<1)
-                        {
-                            $output .= "<h3 class = 'text-center'>No image uploaded</h3>";
-                        }
-                        else
-                        {
-                            echo "hehe";
-                            while($row = mysqli_fetch_assoc($que))
-                            {
-                                $temp = $row['image'];
-                                ?>
-                                <img src="$temp" class='my-3' style = 'width:400px;height:400px;'>
-                                <?php
-                            }
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
+  	$sql = "INSERT INTO student_table (imagename, contact) VALUES ('$Get_image_name', 'USA')";
+  	
+	// Run SQL query
+  	mysqli_query($conn, $sql);
 
-    </div>
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $image_Path)) {
+  		echo "Your Image uploaded successfully";
+  	}else{
+  		echo  "Not Insert Image";
+  	}
+    }
+  
+  
+    // Fetch image from database
+	$img = mysqli_query($conn, "SELECT * FROM student_table");
+     while ($row = mysqli_fetch_array($img)) {     
+		
+      	echo "<img src='images/".$row['imagename']."' style = 'width:400px;height:400px;' >";   
+      
+    }     
+?>
 </body>
 </html>
