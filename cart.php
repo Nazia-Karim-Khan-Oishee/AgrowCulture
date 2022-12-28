e1b1e86d898a8d060a0dfd4b347418cff81e84b8
sami <ittehadrahman@iut-dhaka.edu>
2022-12-28 21:39:07 +0600

    sssss

--- cart.php
+++ cart.php
@@ -33,8 +33,8 @@
     {
         if(mysqli_num_rows(mysqli_query($Conn,"Select * from `temporary`"))==0)
         {
-            echo "Cart is empty";
-
+            // echo "Cart is empty";
+            $message="Cart is empty";
         }
         else{
 
