aa8574332b8f3b71a321e382af3409bc6a196f70
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2022-12-30 01:33:33 +0600

    modify

--- Review.php
+++ Review.php
@@ -92,7 +92,7 @@ $user_name = $_SESSION['user_name'];
             </div>
             <ul class="links-container">
             <li class="link-item"><a href="getstartedpage.php" class="link">HOME</a></li>
-            <li class="link-item"><a href="4optionss" class="link">SERVICES</a></li>
+            <li class="link-item"><a href="4optionss.php" class="link">SERVICES</a></li>
             <li class="link-item"><a href="purchase.php" class="link">PURCHASE</a></li>
             <li class="link-item"><a href="crops.php" class="link">CROPS</a></li>
             <li class="link-item"><a href="vegetables.php" class="link">VEGETABLES</a></li>
